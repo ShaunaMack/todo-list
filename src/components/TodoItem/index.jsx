512af1acd906b0f9d8836c362f53c0322f670fbf
Shauna Mack <shaunakatemack@gmail.com>
2024-11-20 14:04:40 +1000

    add comment for destructuring props

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,5 +1,8 @@
 function TodoItem(props) {
+  // use destructuring to get access to things passed in through props
+  // this is instead of using props.text where we want to have the text shown:x
   const { text } = props;
+
   return <div>{text}</div>;
 }
 
