a26ac9c3e773710fed190089cb52a2ba41880a54
Shauna Mack <shaunakatemack@gmail.com>
2024-11-20 14:46:59 +1000

    render a complete button for each todo item

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,10 +2,17 @@ import "./style.css";
 
 function TodoItem(props) {
   // use destructuring to get access to things passed in through props
-  // this is instead of using props.text where we want to have the text shown:x
+  // this is instead of using props.text props.completeTodo and props.index
   const { todo, completeTodo, index } = props;
 
-  return <div className="todo">{todo.text}</div>;
+  return (
+    <div className="todo">
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoItem;
